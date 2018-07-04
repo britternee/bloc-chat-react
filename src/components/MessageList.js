3c8e1465d2b5f9a7aac13a54359614bb251ea0a4
Brittany Miller <britmill@gmail.com>
2018-07-03 22:35:24 -0600

    timestamp conversion success

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -23,13 +23,40 @@ class MessageList extends Component {
         });
     }
 
+    convertTimestamp(timestamp) {
+        var d = new Date(timestamp),	// Convert the passed timestamp to milliseconds
+            yyyy = d.getFullYear(),
+            mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+            dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+            hh = d.getHours(),
+            h = hh,
+            min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+            ampm = 'AM',
+            time;
+	    if (hh > 12) {
+            h = hh - 12;
+            ampm = 'PM';
+	    } else if (hh === 12) {
+            h = 12;
+            ampm = 'PM';
+	    } else if (hh === 0) {
+		    h = 12;
+	}
+
+        time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+        console.log(time)
+        return time;
+    }
+
+
     createMessages(e) {
         e.preventDefault();
         this.messagesRef.push({
             content: this.state.message.content,
             roomId: this.props.activeRoom.key,
             username: this.props.user ? this.props.user.displayName : "Guest",
-            sentAt: this.props.firebase.database.ServerValue.TIMESTAMP
+            //this sentAT is giving me incorrect date and time
+            sentAt: this.convertTimestamp(Date.now())
         });
         this.setState({
             message:{
